96ceb9af5dc722b20c93c4d187f4bbb308435485
cedricvasset <vasset.cedric@orange.fr>
2018-12-15 15:23:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part3 ex5</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <?php for($comp = 1 ; $comp <= 15 ; $comp++) { ?>
+    <p>On y arrive presque.</p>
+  <?php } ?>
+
+    <?php //while($comp <= 15) { ?>
+<!--  <p>On y arrive presque </p> -->
+  <?php // $comp ++
+//} ?>
+</body>
+</html>
